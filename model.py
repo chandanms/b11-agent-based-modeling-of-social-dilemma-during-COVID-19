93c85663c591a668aeec18df0650363dd0758e53
Chandan M S <mschandan96@gmail.com>
2020-09-20 17:03:44 +0200

    Merged base_branch_v1 with master

---
+++ model.py
@@ -0,0 +1,155 @@
+from mesa import Agent, Model
+from mesa.space import SingleGrid, MultiGrid
+from mesa.datacollection import DataCollector
+from mesa.time import RandomActivation
+import numpy as np
+
+import enum
+
+
+## Declare the Main Model of parameters
+
+class MainModel(Model) :
+	def __init__(self, population_density, death_rate, transfer_rate, initial_infection_rate, width, height, recovery_days=21) :
+
+		self.population_density = population_density
+		self.death_rate = death_rate
+		self.width = width
+		self.height = height
+		self.transfer_rate = transfer_rate
+		self.initial_infection_rate = initial_infection_rate
+		self.recovery_days=recovery_days
+		self.incubation_time=4
+		self.dead_agents_number = 0
+
+		self.grid = SingleGrid(width, height, True)
+		self.schedule = RandomActivation(self)
+
+		i = 0
+
+		# Get all the cells in SingleGrid
+
+		for cell in self.grid.coord_iter() :
+			x, y = cell[1], cell[2]
+
+			# Add agents and infect them with initial infection rate
+			
+			if self.random.random() < self.population_density :
+				agent = MainAgent(i, self, (x, y))
+
+				if (np.random.choice([0, 1], p = [1 - self.initial_infection_rate, self.initial_infection_rate])) == 1 :
+					agent.state = InfectionState.INFECTED
+					agent.infected_time=self.schedule.time   # time of infection is added for every agent
+
+				self.grid.position_agent(agent, (x,y))
+				self.schedule.add(agent)
+				i = i + 1
+
+		self.total_population = i
+
+		self.running = True
+		self.datacollector = DataCollector(agent_reporters={"State": "state"})
+
+	# Get number of infected agents
+
+	def get_infection_number(self) :
+		infected_number = 0
+		for cell in self.grid.coord_iter() :
+			if (cell[0] != None) :
+				agent = cell[0]
+				if agent.state == InfectionState.INFECTED :
+					infected_number = infected_number + 1
+		return infected_number
+
+
+	# Get number of recovered agents
+
+	def get_recovered_number(self) :
+		recovered_number = 0
+		for cell in self.grid.coord_iter() :
+			if (cell[0] != None) :
+				agent = cell[0]
+				if agent.state == QuarentineState.FREE :
+					recovered_number = recovered_number + 1
+		return recovered_number
+
+	# Get total dead agents
+
+	def get_dead_number(self) :
+		return self.dead_agents_number
+				
+
+	def step(self) :
+
+		self.datacollector.collect(self)
+		self.schedule.step()
+
+		# Stop the simulation if entire population gets infected
+		
+		if self.get_infection_number() == self.total_population :
+			self.running = False
+
+
+# Infection states. I havent added the state SUSCEPTIBLE since everyone is susceptible to covid19 (Age, asymptomatic parameters will be added in later versions)
+
+class InfectionState(enum.IntEnum) :
+	CLEAN = 0
+	INFECTED = 1
+
+class QuarentineState(enum.IntEnum) :
+	FREE = 2
+	QUARENTINE = 3
+	
+
+class MainAgent(Agent) :
+	def __init__(self, unique_id, model, pos) :
+		super().__init__(unique_id, model)
+		self.state = InfectionState.CLEAN
+		self.infected_time=0
+
+
+
+	# Spreading the virus based on contact with nearby cells
+
+	def spread(self) :
+		possible_spread_list = self.model.grid.get_neighborhood(self.pos, moore=True, include_center=False)
+
+		# If the nearby cell is not empty and transfer probability is below the predefined value, infect the neighbour
+
+		for possible_spread in possible_spread_list :
+			if (self.model.grid.is_cell_empty(possible_spread) == False) and (self.random.random() < self.model.transfer_rate) and (self.state == InfectionState.INFECTED) :
+				agent = self.model.grid.get_cell_list_contents(possible_spread)[0]
+				if (agent.state == InfectionState.CLEAN) :
+					agent.state = InfectionState.INFECTED
+					agent.infected_time=self.model.schedule.time  
+
+
+	def move(self) :
+		# agent moves only if not in quarentine
+		if self.state != QuarentineState.QUARENTINE:
+			self.model.grid.move_to_empty(self)
+
+
+	# update agents from infected/quarentine -> dead, infected/quarentine -> recover, infected -> quarentine
+	def update_status(self):
+		if self.state in [InfectionState.INFECTED,QuarentineState.QUARENTINE] and self.model.recovery_days < self.model.schedule.time-self.infected_time:
+			if np.random.choice([0,1], p=[1-self.model.death_rate,self.model.death_rate]) == 1:
+				self.model.grid.remove_agent(self)
+				self.model.schedule.remove(self)
+				self.model.dead_agents_number = self.model.dead_agents_number + 1
+			self.state = QuarentineState.FREE
+		elif self.state == InfectionState.INFECTED and self.model.incubation_time < self.model.schedule.time-self.infected_time:
+			self.state = QuarentineState.QUARENTINE
+
+		
+	def step(self) :
+		self.spread()
+		self.move()
+		self.update_status()
+
+# model = MainModel(population_density, death_rate, transfer_rate, initial_infection_rate, width, height)
+
+# steps = 10
+
+# for i in range(steps) :
+# 	model.step()
---
+++ server.py
@@ -0,0 +1,74 @@
+from mesa.visualization.ModularVisualization import ModularServer
+from mesa.visualization.modules import CanvasGrid, ChartModule, TextElement
+from mesa.visualization.UserParam import UserSettableParameter
+
+from model import MainModel, InfectionState , QuarentineState
+
+# Text elements to Display
+
+class InfectedTextElement(TextElement) :
+	def __init__(self) :
+		pass
+
+	def render(self, model) :
+		infected_number = model.get_infection_number()
+		return "Infected Agents : " + str(infected_number)
+
+class RecoveredTextElement(TextElement) :
+	def __init__(self) :
+		pass
+
+	def render(self, model) :
+		recovered_number = model.get_recovered_number()
+		return "Recovered Agents : " + str(recovered_number)
+
+class DeadTextElement(TextElement) :
+	def __init__(self) :
+		pass
+
+	def render(self, model) :
+		dead_number = model.get_dead_number()
+		return "Dead Agents : " + str(dead_number)
+
+# Color coding of different states, to improve - add colorcoding to mix of two states. ex : Infected and Quarentine
+
+def draw(agent) :
+	if agent is None :
+		return
+
+	portrayal = {"Shape" : "circle", "r" : 0.5, "Filled" : "true", "Layer" : 0}
+
+	if agent.state == InfectionState.CLEAN :		
+		portrayal["Color"] = ["#0000FF", "#9999FF"]
+	elif agent.state == QuarentineState.FREE :
+		portrayal["Color"] = ["#32CD32", "#7FFF00"]   #green
+	elif agent.state == QuarentineState.QUARENTINE :
+		portrayal["Color"] = ["#FF9000", "#FF7B00"]   #orange
+	else :
+		portrayal["Color"] = ["#FF0000", "#FF9999"]
+		
+
+	return portrayal
+
+
+infected_number_text_element = InfectedTextElement()
+recovered_number_text_element = RecoveredTextElement()
+dead_number_text_element = DeadTextElement()
+canvas_element = CanvasGrid(draw, 20, 20, 500, 500)
+
+# Declare model parameters
+
+model_params = {
+    "height": 20,
+    "width": 20,
+    "population_density" : UserSettableParameter("slider", "Population Density", 0.5, 0.1, 0.8, 0.1),
+    "death_rate" : 0.02,
+    #"transfer_rate" : 0.3,
+    "transfer_rate" : UserSettableParameter("slider", "Virus Transfer Rate", 0.3, 0.1, 0.6, 0.1),
+    #"initial_infection_rate" : 0.02,
+    "initial_infection_rate" : UserSettableParameter("slider", "Initial Infection Rate", 0.02, 0.01, 0.08, 0.01)
+}
+
+server = ModularServer(MainModel,
+                       [canvas_element, infected_number_text_element, recovered_number_text_element, dead_number_text_element],
+                       "Infection Model", model_params)
